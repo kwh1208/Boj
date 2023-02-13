b6336f35bdd465395f28da1b4603cdfbcb94f38a
kwh <kwh871005@gmail.com>
2023-02-13 21:28:17 +0900

    Boj 2407 조합

--- src/Boj_2407.java
+++ src/Boj_2407.java
@@ -2,6 +2,7 @@ import java.math.BigInteger;
 import java.util.Scanner;
 
 public class Boj_2407 {
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
