ee03cb8a1d5de772487165a410504e38f4e54969
kwh <kwh871005@gmail.com>
2023-02-18 22:49:48 +0900

    Boj 2304 창고다각형

--- src/Boj_2304.java
+++ src/Boj_2304.java
@@ -4,6 +4,7 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Boj_2304 {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
