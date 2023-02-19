198320d5ff08d6595de812d0fbc080c4e94ef646
kwh <kwh871005@gmail.com>
2023-02-19 11:07:50 +0900

    Boj 2467 용액

--- src/Boj_2467.java
+++ src/Boj_2467.java
@@ -13,20 +13,20 @@ public class Boj_2467 {
         for (int i = 0; i < n; i++) {
             acid[i] = Integer.parseInt(str1.nextToken());
         }
-        int s = 0;
-        int e = n-1;
+        int s = 0;//앞에서 시작
+        int e = n-1;//뒤에서 시작
         int dif = Integer.MAX_VALUE;
         int ans1 = 0;
         int ans2 = 0;
         while (true){
-            if(s>=e) break;
+            if(s>=e) break;//중간에서 만나거나 서로 교차되면 끝.
             int sum = acid[s]+acid[e];
             if(Math.abs(sum)<dif) {dif = Math.abs(sum);ans1 = acid[s];ans2 = acid[e];}
             if(sum==0) break;
-            else if (sum > 0) {
+            else if (sum > 0) {//합이 크면 전체 수를 줄여줘야하므로 e를 한칸 땡겨줌.
                 e--;
             }
-            else {s++;}
+            else {s++;}//합이 작으면 전체 수를 키워줘야하므로 s를 한칸 땡겨줌.
 
         }
         System.out.println(ans1+" "+ans2);
