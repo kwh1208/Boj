8e58113bad4463f1561a16f4ac95333d0b0ed38d
kwh1208 <117095342+kwh1208@users.noreply.github.com>
2023-02-20 13:21:26 +0900

    Update and rename Boj_2259.java to Boj_2559.java

--- src/Boj_2559.java
+++ src/Boj_2559.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Boj_2259 {
+public class Boj_2559 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer str = new StringTokenizer(br.readLine());
