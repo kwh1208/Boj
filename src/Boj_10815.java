8f18e2d8513caeca2bab49a8ca67f78732158189
kwh <kwh871005@gmail.com>
2023-03-21 22:06:08 +0900

    Boj 10815 숫자 카드

--- src/Boj_10815.java
+++ src/Boj_10815.java
@@ -15,19 +15,19 @@ public class Boj_10815 {
         for (int i = 0; i < n; i++) {
             num.add(Integer.parseInt(str.nextToken()));
         }
-        Collections.sort(num);
+        Collections.sort(num);//Collection을 이용하는게 정렬이 훨씬 빠릅니다.
         int m = Integer.parseInt(br.readLine());
         str = new StringTokenizer(br.readLine());
         for (int i = 0; i < m; i++) {
-            int tmp = Integer.parseInt(str.nextToken());
-            int s = 0;
-            int e = n-1;
+            int tmp = Integer.parseInt(str.nextToken());//내가 가지고 있는 카드인가?
+            int s = 0; //첫번째 카드
+            int e = n-1;//마지막 카드
             while (true){
                 int mid = (s+e)/2;
-                if(s>e) {sb.append(0).append(" ");break;}
-                if(num.get(mid)==tmp) {sb.append(1).append(" ");break;}
-                if(num.get(mid)>tmp) {e=mid-1;continue;}
-                if(num.get(mid)<tmp) {s=mid+1;}
+                if(s>e) {sb.append(0).append(" ");break;}//다돌았는데 없네? 0 출력
+                if(num.get(mid)==tmp) {sb.append(1).append(" ");break;}//가지고 있네? 1 출력
+                if(num.get(mid)>tmp) {e=mid-1;continue;}// mid가 확인할 숫자보다 크네? mid가 작아져야겠군. -> e 줄여야됨.
+                if(num.get(mid)<tmp) {s=mid+1;}// mid가 확인할 숫자보다 작군. mid가 커져야겠군 -> s 늘려야됨.
             }
         }
         System.out.println(sb);
