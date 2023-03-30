5fc18603d2144e08bce2764c11a85218f68c2b97
kwh <kwh871005@gmail.com>
2023-03-30 23:54:54 +0900

    Boj 1487 물건팔기

---
+++ src/Boj_1487.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Boj_1487 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] buy_price = new int[n];
+        int[] order_price = new int[n];
+        StringTokenizer str;
+        for (int i = 0; i < n; i++) {
+            str = new StringTokenizer(br.readLine());
+            buy_price[i] = Integer.parseInt(str.nextToken());
+            order_price[i] = Integer.parseInt(str.nextToken());
+        }
+        int max = 0;
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            int sell_price = buy_price[i];
+            int total = 0;
+            for (int j = 0; j < n; j++) {
+                if(sell_price<=buy_price[j]&&sell_price>order_price[j]){
+                    total+=sell_price-order_price[j];
+                }
+            }
+            if(total>max) {
+                max = total;
+                ans = sell_price;}
+        }
+        System.out.println(ans);
+    }
+}
